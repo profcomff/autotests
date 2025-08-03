2e443e83627b678f9dee5e553a91e239af6f3589
GinKo314 <woolf20050@gmail.com>
2025-08-03 22:50:02 +0300

    initial

--- tests/rating/QA104/conftest.py
+++ tests/rating/QA104/conftest.py
@@ -8,9 +8,14 @@ load_dotenv()
 # Загружает переменные из .env файла
 
 # Теперь их можно использовать в тестах
-API_TOKEN = os.getenv("API_TOKEN")
+BASE_API_TOKEN_RATING = os.getenv("API_TOKEN_RATING")
+print(BASE_API_TOKEN_RATING)
 BASE_URL = os.getenv("BASE_URL")
-
+print(BASE_URL)
+API_TOKEN_USER_A = os.getenv("API_TOKEN_USER_A")
 BASE_HEADERS = {
-    "Authorization": API_TOKEN
+    "Authorization": BASE_API_TOKEN_RATING
+}
+HEADERS_USER_A = {
+    "Authorization": API_TOKEN_USER_A
 }
